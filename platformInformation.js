7e5c2db99862163e18f978419f3f7bd54dac8d2c
Harry <harutsargsyan46@gmail.com>
2022-04-06 14:31:17 +0400

    generateCard function

--- platformInformation.js
+++ platformInformation.js
@@ -1,4 +1,4 @@
-export const platformInformation = [
+const platformInformation = [
   {
     icon: "",
     title: "Community Edition",
@@ -48,3 +48,20 @@ export const platformInformation = [
       "Visually explore your data through a free-form drag-and-drop canvas, a broad range of modern data visualizations, and an easy-to-use report authoring experience.",
   },
 ];
+
+const generateCard = (icon, title, description) => {
+  return `
+  <div class="card_box">
+      <div cass="card_img"></div>
+      <h2 class="card_title">${title.toUpperCase()} ${title}</h2>
+      <p class="card_description">${description}</p>
+  </div>
+
+  `;
+};
+
+const cardList = platformInformation
+  .map((card) => {
+    return generateCard(card.icon, card.title, card.description);
+  })
+  .join("");
